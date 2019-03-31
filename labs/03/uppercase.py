6925ec295c6c4307e74bf559d9a7248117fb57d9
Tomas Karella <tomas.karella@gmail.com>
2019-03-31 19:53:06 +0200

    first dummy solution

--- labs/03/uppercase.py
+++ labs/03/uppercase.py
@@ -9,16 +9,26 @@ import tensorflow as tf
 
 from uppercase_data import UppercaseData
 
+# to upper
+def to_upper_on(str, indices):
+    res = ""
+    for s,i in zip(str, indices):
+        if i > 0.5:
+            res += s.upper()
+        else:
+            res += s
+    return res
+
 # Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--alphabet_size", default=0, type=int, help="If nonzero, limit alphabet to this many most frequent chars.")
+parser.add_argument("--alphabet_size", default=30, type=int, help="If nonzero, limit alphabet to this many most frequent chars.")
 parser.add_argument("--learning_rate", default=0.01, type=float, help="Initial learning rate.")
 parser.add_argument("--hidden_layer", default=200, type=int, help="Size of the hidden layer.")
-parser.add_argument("--batch_size", default=1024, type=int, help="Batch size.")
+parser.add_argument("--batch_size", default=512, type=int, help="Batch size.")
 parser.add_argument("--epochs", default=10, type=int, help="Number of epochs.")
 parser.add_argument("--hidden_layers", default="500", type=str, help="Hidden layer configuration.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-parser.add_argument("--window", default=10, type=int, help="Window size to use.")
+parser.add_argument("--threads", default=8, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--window", default=2, type=int, help="Window size to use.")
 args = parser.parse_args()
 args.hidden_layers = [int(hidden_layer) for hidden_layer in args.hidden_layers.split(",") if hidden_layer]
 
@@ -51,17 +61,21 @@ dev_labels = uppercase_data.dev.data["labels"]
 # print(dev_labels.shape) 
 # print(batch_size)
 
-inputs = tf.keras.layers.Input(shape=(2 * args.window + 1), dtype=tf.int32)
+
+inputs = tf.keras.layers.Input(shape=[2 * args.window + 1], dtype=tf.int32)
 encoded = tf.one_hot(inputs, len(uppercase_data.train.alphabet))
 flattened = tf.keras.layers.Flatten()(encoded)
-outputs = tf.keras.layers.Dense(1, activation=tf.nn.softmax)(flattened)
-model = tf.keras.Model(inputs=inputs, outputs=outputs) 
-
-print(inputs.shape)
+hidden1 = tf.keras.layers.Dense(156, activation=tf.nn.relu)(flattened)
+dropout1 = tf.keras.layers.Dropout(0.75)(hidden1)
+hidden2 = tf.keras.layers.Dense(156, activation=tf.nn.relu)(dropout1)
+dropout2 = tf.keras.layers.Dropout(0.75)(hidden2)
+outputs = tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)(dropout2)
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
+print(len(uppercase_data.train.alphabet))
 model.compile(
-        optimizer=tf.optimizers.Adam(learning_rate=args.learning_rate),
-        loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-        metrics=[tf.keras.metrics.BinaryAccuracy()],
+        optimizer=tf.optimizers.Adam(),
+        loss=tf.keras.losses.BinaryCrossentropy(),
+        metrics=["accuracy"],
 )
 
 model.fit(
@@ -92,8 +106,11 @@ model.fit(
 # - Alternatively, you can use `tf.keras.layers.Embedding`, which is an efficient
 #   implementation of one-hot encoding followed by a Dense layer, and flatten afterwards.
 
+test_data = uppercase_data.test.data["windows"]
+pred = model.predict(test_data)
+print("1",np.sum(pred),"0", len(pred))
 with open("uppercase_test.txt", "w", encoding="utf-8") as out_file:
     # TODO: Generate correctly capitalized test set.
     # Use `uppercase_data.test.text` as input, capitalize suitable characters,
     # and write the result to `uppercase_test.txt` file.
-    pass
+    out_file.write(to_upper_on(uppercase_data.test.text, pred))
